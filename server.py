26bae7f0dd38871cc2fb96736188d3db693669d7
Maastrup <karljohan@maastrup.dk>
2019-03-19 10:22:15 +0100

    Now client cannot '\quit' before choosing name

--- server.py
+++ server.py
@@ -41,7 +41,9 @@ def handle_client(client, addr):
 
     name = client.recv(1024)
 
-    if name == b'{GHOST}' or name == b'\quit':
+    print('Name is \'{}\''.format(name))
+
+    if name == b'{GHOST}': # or name == b'\quit':
         return
 
     lock.acquire()
